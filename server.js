54ac57ff761140ad98e741043ee9d801ddfa1fdf
Henrique Dias Campos <henrique.campos@mv.com.br>
2021-10-12 00:51:44 -0300

    teste de redirecionamento

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4200;
 const PATH = `${__dirname}/dist/easy-cipher-front`;
+const $URL_API = process.env.URL_API;
 
 app.use(express.static(PATH));
 
 app.get('/*', ( req, res) => {
-    res.sendFile(`${PATH}/index.html`)
+    if(/^\/api\/.*$/.test(req.url)) {
+        console.log(`redirecionando para ${$URL_API}${req.url}`);
+        res.redirect(`${$URL_API}${req.url}`);
+    } else {
+        res.sendFile(`${PATH}/index.html`)
+    }
 });
 
 app.listen(PORT, () => {
-    console.log(`start server on port ${PORT}`);
+    console.log(`start server on port ${PORT} ${$URL_API}`);
 })
